1b16a109ffdb686182fd1d256a5e1e7c04af8dbb
Gill <peter.gill@accenture.com>
2020-12-05 14:37:23 +1100

    changed db connection string, not ssl, corrected

--- src/main/java/InventoryParse/ParseJSON.java
+++ src/main/java/InventoryParse/ParseJSON.java
@@ -199,7 +199,7 @@ public class ParseJSON {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company/useSSL=false", "storeuser",
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company?useSSL=false", "storeuser",
 					"storeuser");
 
 			Statement stmt = con.createStatement();
