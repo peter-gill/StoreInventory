217252d470cc5d0cc39857e04a88e3276e8c12f3
peter-gill <mrpetergill@yahoo.com.au>
2020-12-05 04:16:40 +0000

    Updated mysql driver version

--- src/main/java/InventoryParse/ParseJSON.java
+++ src/main/java/InventoryParse/ParseJSON.java
@@ -199,13 +199,16 @@ public class ParseJSON {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company?useSSL=false", "storeuser",
+			//Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company?useSSL=false", "storeuser",
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/company?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT&useSSL=false", "storeuser",
 					"storeuser");
 
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from products");
+			ResultSet rs = stmt.executeQuery("show columns from products");
+			//ResultSet rs = stmt.executeQuery("select * from products");
 			while (rs.next())
-				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+				//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+				System.out.println(rs.getString(1));
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
