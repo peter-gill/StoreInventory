cba0643fbe4a1a8380adc1a4e0542ac5a5409e11
Gill <peter.gill@accenture.com>
2020-12-05 12:49:43 +1100

    playing with DB connection

--- src/main/java/InventoryParse/ParseJSON.java
+++ src/main/java/InventoryParse/ParseJSON.java
@@ -3,6 +3,10 @@ package InventoryParse;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONArray;
@@ -35,6 +39,8 @@ public class ParseJSON {
 
 	public void findParseJSON() throws Exception {
 
+		connectToMySqlDB();
+		
 		for (;;) {
 
 			File absoluteFileName = findFile();
@@ -136,7 +142,7 @@ public class ParseJSON {
 
 		}
 
-		//System.out.println("######## Closing " + absoluteFileName.getName());
+		// System.out.println("######## Closing " + absoluteFileName.getName());
 
 		fileReader.close();
 
@@ -187,6 +193,23 @@ public class ParseJSON {
 			System.out.println("Unable to move to Processed Directory");
 		}
 
+	}
+
+	private void connectToMySqlDB() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "storeuser",
+					"storeuser");
+
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from products");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
